25d93ccb72528a8771ea814ec6def018ddfc4bf9
David L. Inman <davidlevere@gmail.com>
2023-11-18 18:46:06 -0500

    resolve todo

--- src/parsing.rs
+++ src/parsing.rs
@@ -3,7 +3,6 @@ use std::iter::Peekable;
 
 use crate::data::*;
 
-// TODO can probably have a Records iterator 
 pub fn parse_records(input : &mut impl Iterator<Item = char>, options : &Options) -> Result<Vec<Record>, String> {
     let mut input = input.peekable();
 
